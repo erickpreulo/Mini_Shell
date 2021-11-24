afd20b54aeab99ae3dcd70cd2cf264ec84d2b76d
Erick Gomes <erickgomes@Mac-mini-de-Erick.local>
2021-11-24 18:51:37 +0000

    bla

---
+++ study/dir.c
@@ -0,0 +1,13 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+int		main(int argc, char **argv)
+{
+	
+}
--- study/dupdup2.c
+++ study/dupdup2.c
@@ -2,8 +2,30 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
 int	main(int ac, char **av)
 {
-	
+	int pid = fork();
+	if (pid == -1)
+	{
+		return 1;
+	}
+	if (pid == 0)
+	{
+		int file = open("pingResults.txt", O_WRONLY | O_CREAT, 0777);
+		if (file == -1)
+		{
+			return 2;
+		}
+		printf("The pingresult fd: %d\n", file);
+		int file2 = dup2(file, STDOUT_FILENO);
+		printf("The duplicate fd: %d\n", file2);
+		close(file);
+	}
 }
+
+// e´ uma opçao para redirecionar o standard output
